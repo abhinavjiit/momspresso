a97678b53551701375ed571675c5cab7d377d00d
Hemant <hemant.parmar@mycity4kids.com>
2018-10-05 10:09:02 +0530

    Search history and trending result changes

--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/retrofitAPIsInterfaces/SearchArticlesAuthorsAPI.java
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/retrofitAPIsInterfaces/SearchArticlesAuthorsAPI.java
@@ -1,13 +1,14 @@
 package com.mycity4kids.retrofitAPIsInterfaces;
 
-import com.mycity4kids.models.parentingdetails.ParentingDetailResponse;
-import com.mycity4kids.models.parentingstop.CommonParentingResponse;
+import com.mycity4kids.models.request.SaveSearchQueryRequest;
 import com.mycity4kids.models.response.SearchResponse;
-import com.mycity4kids.models.response.SearchTopicResult;
-import com.mycity4kids.newmodels.bloggermodel.ParentingBlogResponse;
+import com.mycity4kids.models.response.SearchTrendsAndHistoryResponse;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -60,4 +61,9 @@ public interface SearchArticlesAuthorsAPI {
                                                           @Query("end") int end,
                                                           @Query("city") String cityId);
 
+    @GET("/v1/search/usersearch")
+    Call<SearchTrendsAndHistoryResponse> getSearchTrendAndHistory();
+
+    @POST("/v1/search/insert")
+    Call<ResponseBody> saveSearchQuery(@Body SaveSearchQueryRequest saveSearchQueryRequest);
 }
--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/activity/SearchAllActivity.java
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/activity/SearchAllActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,19 +18,30 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.crashlytics.android.Crashlytics;
 import com.kelltontech.network.Response;
 import com.kelltontech.ui.BaseActivity;
 import com.kelltontech.utils.StringUtils;
 import com.mycity4kids.R;
+import com.mycity4kids.application.BaseApplication;
 import com.mycity4kids.constants.Constants;
 import com.mycity4kids.gtmutils.Utils;
+import com.mycity4kids.models.request.SaveSearchQueryRequest;
+import com.mycity4kids.models.response.SearchTrendsAndHistoryResponse;
 import com.mycity4kids.preference.SharedPrefUtils;
+import com.mycity4kids.retrofitAPIsInterfaces.SearchArticlesAuthorsAPI;
 import com.mycity4kids.ui.adapter.SearchAllPagerAdapter;
 import com.mycity4kids.utils.AppUtils;
 
 import java.util.TimerTask;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
 
 /**
  * Created by hemant on 19/4/16.
@@ -45,6 +57,9 @@ public class SearchAllActivity extends BaseActivity implements View.OnClickListe
     String searchParam;
     int tabPosition;
     Handler handler = new Handler();
+    private LinearLayout searchHistoryAndTrendContainer;
+    private TextView userSearchHistory1TextView, userSearchHistory2TextView, trendingSearch1TextView, trendingSearch2TextView, trendingSearch3TextView;
+    private View underline1, underline2, underline3, underline4, underline5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +76,24 @@ public class SearchAllActivity extends BaseActivity implements View.OnClickListe
         mSlidingTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         searchImageView = (ImageView) findViewById(R.id.searchImageView);
         searchEditText = (EditText) findViewById(R.id.searchEditText);
+        searchHistoryAndTrendContainer = (LinearLayout) findViewById(R.id.searchHistoryAndTrendContainer);
+        userSearchHistory1TextView = (TextView) findViewById(R.id.userSearchHistory1TextView);
+        userSearchHistory2TextView = (TextView) findViewById(R.id.userSearchHistory2TextView);
+        trendingSearch1TextView = (TextView) findViewById(R.id.trendingSearch1TextView);
+        trendingSearch2TextView = (TextView) findViewById(R.id.trendingSearch2TextView);
+        trendingSearch3TextView = (TextView) findViewById(R.id.trendingSearch3TextView);
+        underline1 = findViewById(R.id.underline1);
+        underline2 = findViewById(R.id.underline2);
+        underline3 = findViewById(R.id.underline3);
+        underline4 = findViewById(R.id.underline4);
+        underline5 = findViewById(R.id.underline5);
+
+        userSearchHistory1TextView.setOnClickListener(this);
+        userSearchHistory2TextView.setOnClickListener(this);
+        trendingSearch1TextView.setOnClickListener(this);
+        trendingSearch2TextView.setOnClickListener(this);
+        trendingSearch3TextView.setOnClickListener(this);
+
         tabsPagerAdapter = new SearchAllPagerAdapter(getSupportFragmentManager(), this, null, this, searchParam);
 
         setSupportActionBar(mToolbar);
@@ -85,36 +118,105 @@ public class SearchAllActivity extends BaseActivity implements View.OnClickListe
             }
         });
 
-//        searchEditText.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                handler.postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        runOnUiThread(new TimerTask() {
-//                            @Override
-//                            public void run() {
-//                                requestSearch();
-//                            }
-//                        });
-//                    }
-//                }, 500);
-//            }
-//        });
+        getUserHistoryAndTrendingSearchResult();
 
         AppUtils.changeTabsFont(this, mSlidingTabLayout);
         searchImageView.setOnClickListener(this);
     }
+
+    private void getUserHistoryAndTrendingSearchResult() {
+        Retrofit retrofit = BaseApplication.getInstance().getRetrofit();
+        SearchArticlesAuthorsAPI searchArticlesAuthorsAPI = retrofit.create(SearchArticlesAuthorsAPI.class);
+        Call<SearchTrendsAndHistoryResponse> call = searchArticlesAuthorsAPI.getSearchTrendAndHistory();
+        call.enqueue(searchHistoryResponseCallback);
+    }
+
+    private Callback<SearchTrendsAndHistoryResponse> searchHistoryResponseCallback = new Callback<SearchTrendsAndHistoryResponse>() {
+        @Override
+        public void onResponse(Call<SearchTrendsAndHistoryResponse> call, retrofit2.Response<SearchTrendsAndHistoryResponse> response) {
+            if (response == null || response.body() == null) {
+                showToast(getString(R.string.server_went_wrong));
+                return;
+            }
+
+            try {
+                SearchTrendsAndHistoryResponse responseData = response.body();
+                if (responseData.getCode() == 200 && Constants.SUCCESS.equals(responseData.getStatus())) {
+                    if (responseData.getData().getResult().getUserResult().size() == 0) {
+                        if (responseData.getData().getResult().getTrendingResult().size() > 2) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch2TextView.setVisibility(View.VISIBLE);
+                            underline4.setVisibility(View.VISIBLE);
+                            trendingSearch3TextView.setVisibility(View.VISIBLE);
+                            underline5.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                            trendingSearch2TextView.setText(responseData.getData().getResult().getTrendingResult().get(1));
+                            trendingSearch3TextView.setText(responseData.getData().getResult().getTrendingResult().get(2));
+                        } else if (responseData.getData().getResult().getTrendingResult().size() == 2) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch2TextView.setVisibility(View.VISIBLE);
+                            underline4.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                            trendingSearch2TextView.setText(responseData.getData().getResult().getTrendingResult().get(1));
+                        } else if (responseData.getData().getResult().getTrendingResult().size() == 1) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                        } else {
+                            searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                        }
+                    } else if (responseData.getData().getResult().getUserResult().size() == 1) {
+                        userSearchHistory1TextView.setVisibility(View.VISIBLE);
+                        underline1.setVisibility(View.VISIBLE);
+                        userSearchHistory1TextView.setText(responseData.getData().getResult().getUserResult().get(0));
+                        if (responseData.getData().getResult().getTrendingResult().size() > 1) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch2TextView.setVisibility(View.VISIBLE);
+                            underline4.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                            trendingSearch2TextView.setText(responseData.getData().getResult().getTrendingResult().get(1));
+                        } else if (responseData.getData().getResult().getTrendingResult().size() == 1) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                        } else {
+
+                        }
+                    } else {
+                        userSearchHistory1TextView.setVisibility(View.VISIBLE);
+                        underline1.setVisibility(View.VISIBLE);
+                        userSearchHistory2TextView.setVisibility(View.VISIBLE);
+                        underline2.setVisibility(View.VISIBLE);
+                        userSearchHistory1TextView.setText(responseData.getData().getResult().getUserResult().get(0));
+                        userSearchHistory2TextView.setText(responseData.getData().getResult().getUserResult().get(1));
+                        if (responseData.getData().getResult().getTrendingResult().size() >= 1) {
+                            trendingSearch1TextView.setVisibility(View.VISIBLE);
+                            underline3.setVisibility(View.VISIBLE);
+                            trendingSearch1TextView.setText(responseData.getData().getResult().getTrendingResult().get(0));
+                        } else {
+
+                        }
+                    }
+
+                } else {
+
+                }
+            } catch (Exception e) {
+                showToast(getString(R.string.server_went_wrong));
+                Crashlytics.logException(e);
+                Log.d("MC4kException", Log.getStackTraceString(e));
+            }
+        }
+
+        @Override
+        public void onFailure(Call<SearchTrendsAndHistoryResponse> call, Throwable t) {
+            Crashlytics.logException(t);
+            Log.d("MC4kException", Log.getStackTraceString(t));
+        }
+    };
 
     @Override
     protected void updateUi(Response response) {
@@ -125,9 +227,66 @@ public class SearchAllActivity extends BaseActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.searchImageView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
                 requestSearch();
                 break;
+            case R.id.userSearchHistory1TextView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
+                searchEditText.setText(userSearchHistory1TextView.getText());
+                requestSearch();
+//                requestSearchWithParam(userSearchHistory1TextView.getText().toString());
+                break;
+            case R.id.userSearchHistory2TextView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
+                searchEditText.setText(userSearchHistory2TextView.getText());
+                requestSearch();
+//                requestSearchWithParam(userSearchHistory2TextView.getText().toString());
+                break;
+            case R.id.trendingSearch1TextView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
+                searchEditText.setText(trendingSearch1TextView.getText());
+                requestSearch();
+//                requestSearchWithParam(trendingSearch1TextView.getText().toString());
+                break;
+            case R.id.trendingSearch2TextView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
+                searchEditText.setText(trendingSearch2TextView.getText());
+                requestSearch();
+//                requestSearchWithParam(trendingSearch2TextView.getText().toString());
+                break;
+            case R.id.trendingSearch3TextView:
+                searchHistoryAndTrendContainer.setVisibility(View.GONE);
+                mSlidingTabLayout.setVisibility(View.VISIBLE);
+                searchEditText.setText(trendingSearch3TextView.getText());
+                requestSearch();
+//                requestSearchWithParam(trendingSearch3TextView.getText().toString());
+                break;
         }
+    }
+
+    private void storeSearchQuery(String query) {
+        SaveSearchQueryRequest saveSearchQueryRequest = new SaveSearchQueryRequest();
+        saveSearchQueryRequest.setQuery(query);
+
+        Retrofit retrofit = BaseApplication.getInstance().getRetrofit();
+        SearchArticlesAuthorsAPI searchArticlesAuthorsAPI = retrofit.create(SearchArticlesAuthorsAPI.class);
+        Call<ResponseBody> call = searchArticlesAuthorsAPI.saveSearchQuery(saveSearchQueryRequest);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 
     private void requestSearch() {
@@ -135,6 +294,16 @@ public class SearchAllActivity extends BaseActivity implements View.OnClickListe
 //            showToast("Please enter a valid search parameter");
         } else {
             tabsPagerAdapter.refreshArticlesAuthors(searchEditText.getText().toString(), mViewPager.getCurrentItem());
+            storeSearchQuery(searchEditText.getText().toString());
+        }
+    }
+
+    private void requestSearchWithParam(String param) {
+        if (StringUtils.isNullOrEmpty(param)) {
+//            showToast("Please enter a valid search parameter");
+        } else {
+            tabsPagerAdapter.refreshArticlesAuthors(param, mViewPager.getCurrentItem());
+            storeSearchQuery(param);
         }
     }
 
