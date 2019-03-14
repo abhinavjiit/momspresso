c41ec8f0d6470aadf627ea51fb8df1f3c80e8241
abhinav <mac@MACs-MacBook-Pro-3.local>
2019-03-14 16:05:19 +0530

    rules add

--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/fragment/ChallengeCategoryVideoTabFragment.java
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/fragment/ChallengeCategoryVideoTabFragment.java
@@ -33,6 +33,7 @@ public class ChallengeCategoryVideoTabFragment extends BaseFragment implements V
     private ArrayList<String> Display_Name = new ArrayList<String>();
     private ArrayList<String> activeImageUrl = new ArrayList<String>();
     private ArrayList<String> activeStreamUrl = new ArrayList<String>();
+    private ArrayList<String> rules = new ArrayList<>();
 
     private ChallengeVideoRecycleAdapter recyclerAdapter;
     private String jsonMyObject;
@@ -52,7 +53,7 @@ public class ChallengeCategoryVideoTabFragment extends BaseFragment implements V
 
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_challenge);
         llm = new LinearLayoutManager(getActivity());
-        recyclerAdapter = new ChallengeVideoRecycleAdapter(this, getActivity(), challengeId, Display_Name, activeImageUrl, activeStreamUrl);
+        recyclerAdapter = new ChallengeVideoRecycleAdapter(this, getActivity(), challengeId, Display_Name, activeImageUrl, activeStreamUrl, rules);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(recyclerAdapter);
