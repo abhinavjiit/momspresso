3f96aeb1e1957e0f16f2921c2abcd514a86820c0
baps <anoop.ghildiyal@mycity4kids.com>
2019-03-15 18:24:28 +0530

    merged with all

--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/adapter/VideoRecyclerViewAdapter.java
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/adapter/VideoRecyclerViewAdapter.java
@@ -171,7 +171,7 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter<BaseViewHolde
         public ImageView mCover, heart, share, whatsapp, three_dot, comment;
         public ProgressBar mProgressBar;
         public final View parent;
-        ImageView userImage,comment;
+        ImageView userImage;
 
 
         public ViewHolder(View itemView) {
