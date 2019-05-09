5e2e15ce1076672a7b49caeb2a680cebf3ef781e
asdinformatics <ashlok.malik@mycity4kids.com>
2019-05-09 17:26:47 +0530

    campaign detail

--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/campaign/adapter/CampaignDetailAdapter.kt
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/campaign/adapter/CampaignDetailAdapter.kt
@@ -1,16 +1,24 @@
 package com.mycity4kids.ui.adapter
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.support.v4.app.FragmentActivity
 import android.support.v7.widget.RecyclerView
+import android.text.SpannableString
+import android.text.Spanned
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.mycity4kids.R
 import com.mycity4kids.models.campaignmodels.CampaignDetailDeliverable
 import kotlinx.android.synthetic.main.deliverable_list_recycler_adapter.view.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 class CampaignDetailAdapter(private var deliverableList: List<List<CampaignDetailDeliverable>>?, val context: FragmentActivity?) : RecyclerView.Adapter<CampaignDetailAdapter.RewardHolder>() {
 
@@ -28,6 +36,12 @@ class CampaignDetailAdapter(private var deliverableList: List<List<CampaignDetai
 
     inner class RewardHolder(private val view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         private var deliverableList: List<CampaignDetailDeliverable>? = null
+        private val urlPattern = Pattern.compile(
+                "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+                        + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+                        + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
+                Pattern.CASE_INSENSITIVE or Pattern.MULTILINE or Pattern.DOTALL)
+        private var spannable: SpannableString? = null
 
         init {
             view.setOnClickListener(this)
@@ -41,7 +55,8 @@ class CampaignDetailAdapter(private var deliverableList: List<List<CampaignDetai
 
                     builder.append("\u2022" + "  " + instructions + "\n")
                 }
-                (view.deliverable_text).setText(builder.toString())
+                getOffset(builder.toString())
+//                (view.deliverable_text).setText(builder.toString())
                 (view.deliverable_text).setMovementMethod(LinkMovementMethod.getInstance());
                 (view.deliverable_header).setText(deliverableList.get(position).name)
             }
@@ -54,6 +69,30 @@ class CampaignDetailAdapter(private var deliverableList: List<List<CampaignDetai
         }
 
 
+
+        private fun getOffset(instruction: String) {
+            val matcher = urlPattern.matcher(instruction)
+            var matchStart: Int? = null
+            var matchEnd: Int? = null
+            while (matcher.find()) {
+                matchStart = matcher.start(1)
+                matchEnd = matcher.end()
+            }
+            spannable = SpannableString(instruction)
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(p0: View?) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(instruction.substring(matchStart!!, matchEnd!!)))
+                    context!!.startActivity(intent)
+                }
+            }
+            if (matchStart != null && matchEnd != null) {
+                spannable!!.setSpan(clickableSpan, matchStart, matchEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            };
+            (view.deliverable_text).setText(spannable)
+            (view.deliverable_text).isClickable = true
+            (view.deliverable_text).setMovementMethod(LinkMovementMethod.getInstance());
+            (view.deliverable_text).setHighlightColor(Color.TRANSPARENT);
+        }
 
         fun getDate(milliSeconds: Long, dateFormat: String): String {
             // Create a DateFormatter object for displaying date in specified format.
