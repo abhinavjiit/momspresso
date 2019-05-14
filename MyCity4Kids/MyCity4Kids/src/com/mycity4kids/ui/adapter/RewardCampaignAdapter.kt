111cfc27ef658e0e92345fa30e7a31cdf7e560cf
asdinformatics <ashlok.malik@mycity4kids.com>
2019-05-14 18:52:31 +0530

    invite popup

--- MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/adapter/RewardCampaignAdapter.kt
+++ MyCity4Kids/MyCity4Kids/src/com/mycity4kids/ui/adapter/RewardCampaignAdapter.kt
@@ -1,20 +1,31 @@
 package com.mycity4kids.ui.adapter
 
+import android.accounts.NetworkErrorException
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.ShareCompat
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.crashlytics.android.Crashlytics
 import com.mycity4kids.R
 import com.mycity4kids.application.BaseApplication
+import com.mycity4kids.constants.Constants
 import com.mycity4kids.gtmutils.Utils
 import com.mycity4kids.models.campaignmodels.CampaignDataListResult
+import com.mycity4kids.models.campaignmodels.ParticipateCampaignResponse
 import com.mycity4kids.preference.SharedPrefUtils
+import com.mycity4kids.retrofitAPIsInterfaces.CampaignAPI
 import com.mycity4kids.ui.campaign.activity.CampaignContainerActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.campaign_list_recycler_adapter.view.*
+import retrofit2.Call
+import retrofit2.Callback
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -78,7 +89,8 @@ class RewardCampaignAdapter(private var campaignList: List<CampaignDataListResul
             } else {
                 Utils.campaignEvent(context, "Campaign Detail", "Campaign Listing", "Click_listing_card", campaignList!!.name, "android", SharedPrefUtils.getAppLocale(context), SharedPrefUtils.getUserDetailModel(BaseApplication.getAppContext()).dynamoId, System.currentTimeMillis().toString(), "Show_Campaign_Listing")
                 if (campaignList!!.campaignStatus == 8)
-                    Toast.makeText(context, context!!.resources.getString(R.string.toast_campaign_invite), Toast.LENGTH_SHORT).show()
+                    showInviteDialog()
+//                    Toast.makeText(context, context!!.resources.getString(R.string.toast_campaign_invite), Toast.LENGTH_SHORT).show()
                 else
                     (context as CampaignContainerActivity).addCampaginDetailFragment(campaignList!!.id)
 
@@ -168,8 +180,64 @@ class RewardCampaignAdapter(private var campaignList: List<CampaignDataListResul
                 (view.end_date).setText(context!!.resources.getString(R.string.start_date))
                 (view.end_date_text).setText(getDate(campaignList!!.startTime, "dd MMM yyyy"))
             }
+        }
 
+
+        fun showInviteDialog() {
+            if (context != null) {
+                val dialog = Dialog(context)
+                val window = dialog.getWindow()
+                dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
+                dialog.window!!.setGravity(Gravity.CENTER_HORIZONTAL)
+                dialog.setContentView(R.layout.dialog_invite_only_campaign)
+                dialog.setCancelable(true)
+                val okBtn = dialog.findViewById<TextView>(R.id.click_ok)
+                val crossIcon = dialog.findViewById<ImageView>(R.id.cross)
+                crossIcon.setOnClickListener {
+                    dialog.dismiss()
+                }
+                okBtn.setOnClickListener {
+                    (context as CampaignContainerActivity).showProgressDialog(context!!.resources.getString(R.string.please_wait))
+                    val retro = BaseApplication.getInstance().retrofit
+                    val campaignAPI = retro.create(CampaignAPI::class.java)
+                    val call = campaignAPI.postSubscribeCampaign(campaignList!!.id)
+                    call.enqueue(subscribeCampaign)
+                    dialog.dismiss()
+                }
+                dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                dialog.show()
+            }
 
         }
+
+        val subscribeCampaign = object : Callback<ParticipateCampaignResponse> {
+            override fun onResponse(call: Call<ParticipateCampaignResponse>, response: retrofit2.Response<ParticipateCampaignResponse>) {
+                (context as CampaignContainerActivity).removeProgressDialog()
+                if (response == null || null == response.body()) {
+                    val nee = NetworkErrorException(response.raw().toString())
+                    Crashlytics.logException(nee)
+                    return
+                }
+                try {
+                    val responseData = response.body()
+                    if (responseData!!.code == 200 && Constants.SUCCESS == responseData.status) {
+                        Toast.makeText(context, context!!.resources.getString(R.string.toast_campaign_invite_thankyou), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, responseData.reason, Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    Crashlytics.logException(e)
+                    Log.d("MC4kException", Log.getStackTraceString(e))
+                }
+            }
+
+            override fun onFailure(call: Call<ParticipateCampaignResponse>, t: Throwable) {
+                (context as CampaignContainerActivity).removeProgressDialog()
+                Crashlytics.logException(t)
+                Log.d("MC4kException", Log.getStackTraceString(t))
+            }
+        }
+
+
     }
 }
