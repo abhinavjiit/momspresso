de7f4800217ce7f12b5dc78817bf77617727f950
Hemant Parmar <hemant.parmar@mycity4kids.com>
2020-05-15 17:24:47 +0530

    removed unused classes

--- app/src/main/java/com/mycity4kids/ui/adapter/AppSettingsPagerAdapter.java
+++ app/src/main/java/com/mycity4kids/ui/adapter/AppSettingsPagerAdapter.java
@@ -4,22 +4,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-
 import com.mycity4kids.constants.Constants;
-import com.mycity4kids.ui.fragment.ChangePasswordTabFragment;
 import com.mycity4kids.ui.fragment.EditLangPrefsTabFragment;
 import com.mycity4kids.ui.fragment.EditPreferencesTabFragment;
-import com.mycity4kids.ui.fragment.EditProfileTabFragment;
 
 /**
  * Created by hemant on 24/5/17.
  */
 public class AppSettingsPagerAdapter extends FragmentStatePagerAdapter {
+
     private int mNumOfTabs;
-    private EditProfileTabFragment editProfileTabFragment;
     private EditPreferencesTabFragment editPreferencesTabFragment;
     private EditLangPrefsTabFragment editLangPrefsTabFragment;
-    private ChangePasswordTabFragment changePasswordTabFragment;
     private String authorId;
 
     public AppSettingsPagerAdapter(FragmentManager fm, int NumOfTabs, String authorId) {
@@ -33,14 +29,6 @@ public class AppSettingsPagerAdapter extends FragmentStatePagerAdapter {
 
         Bundle bundle = new Bundle();
         switch (position) {
-
-//            case 0:
-//                if (editProfileTabFragment == null) {
-//                    editProfileTabFragment = new EditProfileTabFragment();
-//                }
-//                bundle.putString(Constants.AUTHOR_ID, authorId);
-//                editProfileTabFragment.setArguments(bundle);
-//                return editProfileTabFragment;
             case 0:
                 if (editPreferencesTabFragment == null) {
                     editPreferencesTabFragment = new EditPreferencesTabFragment();
@@ -54,13 +42,6 @@ public class AppSettingsPagerAdapter extends FragmentStatePagerAdapter {
                 }
                 editLangPrefsTabFragment.setArguments(bundle);
                 return editLangPrefsTabFragment;
-//            case 3:
-//                if (changePasswordTabFragment == null) {
-//                    changePasswordTabFragment = new ChangePasswordTabFragment();
-//                }
-//                bundle.putString(Constants.AUTHOR_ID, authorId);
-//                changePasswordTabFragment.setArguments(bundle);
-//                return changePasswordTabFragment;
         }
 
         return null;
