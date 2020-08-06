350387fc6fb0c81a102c9027378a96b8822c2f13
abhinavjiit <abhinav.bhadauria@mycity4kids.com>
2020-08-07 00:47:12 +0530

    block user model changed added @serialized key

--- app/src/main/java/com/mycity4kids/models/BlockUserModel.kt
+++ app/src/main/java/com/mycity4kids/models/BlockUserModel.kt
@@ -1,7 +1,12 @@
 package com.mycity4kids.models
 
+import com.google.gson.annotations.SerializedName
+
 class BlockUserModel(
+    @SerializedName("blocked_user_id")
     var blocked_user_id: String? = null,
+    @SerializedName("blocking_area")
     var blocking_area: IntArray = intArrayOf(1),
+    @SerializedName("reason")
     var reason: String = ""
 )
